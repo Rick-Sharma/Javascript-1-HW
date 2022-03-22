edd0ecb7bdcf973fba820ab70fb9f5801f4dc624
Rick S <100063012+Rick-Sharma@users.noreply.github.com>
2022-03-21 22:19:17 -0700

    Create nice.js
    my java file.

---
+++ nice.js
@@ -0,0 +1,23 @@
+var rick = prompt("What is your name?");
+document.getElementById("Rick").innerHTML = rick;
+
+var sharma = prompt(
+  "Please enter the secret phrase. I know... I know.. you have no idea... It's ok, just guess!"
+);
+document.getElementById("Sharma").innerHTML = sharma;
+
+var march = prompt(
+  "Another popup... I know I hate these too!! Although, your secret phrase was not nice enough.. please enter your favorite thing"
+);
+document.getElementById("March").innerHTML = march + "!!";
+
+var questionFour = prompt(
+  "Seriously .. too much I know this is not fun anymore. Sorry.. but if you could please just guess and enter the correct amount of explamation points.. we will end this madness. "
+);
+document.getElementById("2022").innerHTML = questionFour;
+
+var questionFive = prompt("What is your favorite food");
+document.getElementById("coconut").innerHTML = questionFive;
+
+var questionSix = prompt("If you saw a spooky would you run or eat it?");
+document.getElementById("run").innerHTML = questionSix;
